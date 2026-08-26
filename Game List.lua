47f794e2b6c94713fbaef6ba324cdcf1afbc860b
QuantumNode-js <crewdfx@gmail.com>
2026-08-26 07:53:44 -0600

    Update Game List.lua

--- Game List.lua
+++ Game List.lua
@@ -1,1 +1,6 @@
+local Games = {
+  [9584852943] = "https://www.keyforge.win/v1/load/cmt13933n4sez6sp4lojd6hec?e=1&script=cmta5df2x0fx6zjp4d98skbzm&lsig=bfxXEgwycHiLZakTl4PuzJwybQKaGVEPJJjxwlZZDiY", -- +1 Speed Keyboard Escape
+ [96342491571673] = "https://www.keyforge.win/v1/load/cmt13933n4sez6sp4lojd6hec?e=1&script=cmta5knsf0g5czjp4qhwyxc76&lsig=2Laf197dRMcFaWbbFvsZ5kY1-4OwrRftE-wy0QB_QQo", -- Auto Rng Steal A brainrot
+}
 
+return Games
